f4f22483a9769ac9f1ad9895a2e92e4fb8fbf6a0
MultiW <xinwang.wang@outlook.com>
2021-04-18 04:34:24 +0000

    Uninstall environnment before reinstalling in setup.sh

--- model/setup.sh
+++ model/setup.sh
@@ -1,7 +1,13 @@
 # cd to model/
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 
-conda env create  -f environment.yml
+# uninstall environment
+conda env remove -n model-env
+jupyter kernelspec uninstall -y model-env 
+
+# install environment
+conda env create -f environment.yml
+python -m ipykernel install --user --name model-env --display-name "Python (model-env)"
 
 eval "$(conda shell.bash hook)"
 conda activate model-env
